a6294dcaa96f44811c6aef77019587f2f16422a5
Bogdan <razvant.153957@e.unavarra.es>
2025-07-11 13:35:29 +0200

    Added schema to the enrichedUser endpoint

--- src/schemas/getEnrichedUserSchema.ts
+++ src/schemas/getEnrichedUserSchema.ts
@@ -1,3 +1,62 @@
 export const getEnrichedUserSchema = {
-  // Define your schema here
+  schema: {
+    response: {
+      200: {
+        type: "object",
+        properties: {
+          id: { type: "string" },
+          username: { type: "string" },
+          profile: {
+            type: "object",
+            properties: {
+              bio: { type: "string" },
+              realName: { type: "string" },
+              links: { type: "string" },
+            },
+          },
+          playTime: {
+            type: "object",
+            properties: {
+              total: { type: "number" },
+              tv: { type: "number" },
+            },
+          },
+          rank: { type: "number" },
+          resultStreak: {
+            type: "object",
+            properties: {
+              wins: {
+                type: "object",
+                properties: {
+                  current: { type: "number" },
+                  max: { type: "number" },
+                },
+              },
+              losses: {
+                type: "object",
+                properties: {
+                  current: { type: "number" },
+                  max: { type: "number" },
+                },
+              },
+            },
+          },
+        },
+      },
+      400: {
+        type: "object",
+        properties: {
+          error: { type: "string" },
+        },
+        required: ["error"],
+      },
+      500: {
+        type: "object",
+        properties: {
+          error: { type: "string" },
+        },
+        required: ["error"],
+      },
+    },
+  },
 };
