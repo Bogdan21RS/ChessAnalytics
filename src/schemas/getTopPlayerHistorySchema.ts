1b85d391f866792b3d26524a8f91ab94dc6ada5a
Bogdan <razvant.153957@e.unavarra.es>
2025-07-11 13:38:38 +0200

    Added schema to the topPlayerHistory endpoint

--- src/schemas/getTopPlayerHistorySchema.ts
+++ src/schemas/getTopPlayerHistorySchema.ts
@@ -1,1 +1,36 @@
-export const getTopPlayerHistorySchema = {};
+export const getTopPlayerHistorySchema = {
+  schema: {
+    response: {
+      200: {
+        type: "object",
+        properties: {
+          username: { type: "string" },
+          history: {
+            type: "array",
+            items: {
+              type: "object",
+              properties: {
+                date: { type: "string" },
+                rating: { type: "number" },
+              },
+            },
+          },
+        },
+      },
+      400: {
+        type: "object",
+        properties: {
+          error: { type: "string" },
+        },
+        required: ["error"],
+      },
+      500: {
+        type: "object",
+        properties: {
+          error: { type: "string" },
+        },
+        required: ["error"],
+      },
+    },
+  },
+};
