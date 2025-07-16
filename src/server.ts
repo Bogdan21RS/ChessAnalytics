fef5833b2866fd057ee6477e30204250542afa08
Bogdan <razvant.153957@e.unavarra.es>
2025-07-16 13:59:19 +0200

    Extracted magic ip variable in server code

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
 import { build } from "./serverBuild";
 
 const fastify = build();
+
 const PORT = 5000;
+const setupIp = "0.0.0.0";
 
 const start = async () => {
   try {
-    await fastify.listen({ port: PORT, host: "0.0.0.0" });
+    await fastify.listen({ port: PORT, host: setupIp });
   } catch (err) {
     fastify.log.error(err);
     process.exit(1);
