044a797b624b3f3163775ab8fe94b00f399e91a3
Bogdan <razvant.153957@e.unavarra.es>
2025-07-16 13:29:58 +0200

    Extracted userById code into functions for better readability

--- src/handlers/getUserById.ts
+++ src/handlers/getUserById.ts
@@ -9,39 +9,58 @@ export default async function getUserById(
   lichessUserByIdUrl: string
 ) {
   const { id } = request.query;
-
-  if (queryDoesNotHaveId(id)) {
-    reply.code(400).send({
-      error: INVALID_ID,
-    });
-    return;
+  if (missingIdParameter(id)) {
+    return sendInvalidOrMissingIdResponse(reply);
   }
 
   const userInfoResponse = await getUserInfoGivenId(lichessUserByIdUrl, id);
-
   if (failedResponse(userInfoResponse)) {
-    if (serverError(userInfoResponse)) {
-      reply.code(500).send({
-        error: generalResponseMessages.SERVER_ERROR,
-      });
-      return;
-    }
-    if (userNotFound(userInfoResponse)) {
-      reply.code(404).send({
-        error: generalResponseMessages.USER_NOT_FOUND,
-      });
-      return;
-    }
-
-    reply.code(400).send({
-      error: INVALID_ID,
-    });
-    return;
+    return sendFailedResponse(reply, userInfoResponse);
   }
 
   const userByIdInfo = await userInfoResponse.json();
+  sendUserByIdResponse(reply, userByIdInfo);
+}
 
+function sendFailedResponse(reply: FastifyReply, userInfoResponse: Response) {
+  if (serverError(userInfoResponse)) {
+    return sendServerErrorResponse(reply);
+  }
+  if (userNotFound(userInfoResponse)) {
+    return sendUserNotFoundResponse(reply);
+  }
+
+  return sendInvalidOrMissingIdResponse(reply);
+}
+
+function sendUserNotFoundResponse(reply: FastifyReply) {
+  reply.code(404).send({
+    error: generalResponseMessages.USER_NOT_FOUND,
+  });
+  return;
+}
+
+function sendServerErrorResponse(reply: FastifyReply) {
+  reply.code(500).send({
+    error: generalResponseMessages.SERVER_ERROR,
+  });
+  return;
+}
+
+function sendUserByIdResponse(
+  reply: FastifyReply,
+  userByIdInfo: {
+    [property: string]: Object;
+  }
+) {
   reply.code(200).send(getUserByIdBySpecification(userByIdInfo));
+}
+
+function sendInvalidOrMissingIdResponse(reply: FastifyReply) {
+  reply.code(400).send({
+    error: INVALID_ID,
+  });
+  return;
 }
 
 async function getUserInfoGivenId(lichessUserByIdUrl: string, id: string) {
@@ -62,7 +81,7 @@ function userNotFound(userByIdResponse: Response) {
   return userByIdResponse.status === 404;
 }
 
-function queryDoesNotHaveId(id: string | undefined) {
+function missingIdParameter(id: string | undefined) {
   return !id;
 }
 
